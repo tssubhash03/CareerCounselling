c3c43554009ec1e49dd00fb483b09a9e21523753
tssubhash03 <subhash03ts2004@gmail.com>
2025-02-07 10:54:37 +0530

    Initialize backend with MongoDB connection and basic server setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB(); // Connect to MongoDB
+
+const app = express();
+app.use(express.json()); // Allow send and receive the JSON data.
+app.use(cors()); // Permission receive the data from FRONT END
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
